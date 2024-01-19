743998c63dfcbcce9172672415c44179a5aeddda
Cameron Smith <cameron.ray.smith@gmail.com>
2024-01-19 00:04:12 -0500

    fix(init): do not import modules Signed-off-by: Cameron Smith <cameron.ray.smith@gmail.com>

--- cospar/__init__.py
+++ cospar/__init__.py
@@ -1,4 +1,4 @@
 """CoSpar - dynamic inference by integrating transcriptome and lineage information"""
 
 __version__ = "0.3.0"
-from . import datasets, hf, logging, pl, pp, settings, simulate, tl, tmap
+# from . import datasets, hf, logging, pl, pp, settings, simulate, tl, tmap
